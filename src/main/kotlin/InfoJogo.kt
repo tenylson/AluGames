67d5ecbbee6b8da6dba869fd5be2f1b688ef6434
Luciano Tenylson <tenylson@tjro.jus.br>
2025-01-22 14:42:23 -0400

    Comit inicial - 02.Modelando a aplicação

---
+++ src/main/kotlin/InfoJogo.kt
@@ -0,0 +1,6 @@
+data class InfoJogo(val info:InfoApiShark) {
+
+    override fun toString(): String {
+        return info.toString()
+    }
+}
---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,10 @@
+data class Jogo(val titulo:String, val capa:String) {
+
+
+    val descricao = ""
+    override fun toString(): String {
+        return "Jogo(titulo='$titulo', capa='$capa', descricao='$descricao')"
+    }
+
+
+}
